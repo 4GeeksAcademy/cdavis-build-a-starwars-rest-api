9a4d8e656bd5981f5a1be8ae1237a7981a79f404
Cinthya Davis Gutierrez <cdavis2223@gmail.com>
2024-04-17 02:59:19 +0000

    ya casi faltan 2 endpoints

--- src/app.py
+++ src/app.py
@@ -33,21 +33,60 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET']) #FUNCIONA
 def handle_hello():
-    user = User.query.all()
-    response_body = {
-        'name': user.name,
-        'username': user.username,
-        'lastname': user.lastname,
-        'suscription': user.suscription,
-        'password': user.password,
-        'email': user.email,
-    }
+    users = User.query.all()
+    response_body = []
+    for user in users:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'username': user.username,
+            'lastname': user.lastname,
+            'suscription': user.suscription_dates,
+            'email': user.email,
+            'favorites': user.favorites,
+        }
+        response_body.append(user_data)
 
     return jsonify(response_body), 200
 
-@app.route('/people', methods=['GET'])
+@app.route('/users/<int:user_id>', methods=['GET']) #FUNCIONA
+def get_user(user_id):  
+    user = User.query.filter_by(id=user_id).first()  
+    if user:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'username': user.username,
+            'lastname': user.lastname,
+            'suscription': user.suscription_dates,
+            'email': user.email,
+            'favorites': user.favorites,
+        }
+        return jsonify(user_data), 200
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+    
+
+@app.route('/people/<int:people_id>', methods=['GET']) #FUNCIONA
+def get_person(people_id):
+    person = People.query.get(people_id)
+    if person:
+        person_data = {
+            'name': person.name,
+            'gender': person.gender.type if person.gender else None,
+            'specie': person.specie.language if person.specie else None,
+            'vehicle': person.vehicle.name if person.vehicle else None,
+            'height': person.height,
+            'films': [film.title for film in person.film] if person.film else []
+        }
+        return jsonify(person_data), 200
+    else:
+        return jsonify({'error': 'Person not found'}), 404
+
+@app.route('/people', methods=['GET']) #FUNCIONA
 def get_people():
     people = People.query.all()
     result = []
@@ -55,7 +94,7 @@ def get_people():
         person_data = {
             'name': person.name,
             'gender': person.gender.type if person.gender else None,
-            'specie': person.specie.languaje if person.specie else None,
+            'specie': person.specie.language if person.specie else None,
             'vehicle': person.vehicle.name if person.vehicle else None,
             'height': person.height,
             'films': [film.title for film in person.film] if person.film else []
@@ -63,24 +102,8 @@ def get_people():
         result.append(person_data)
     return jsonify(result), 200
 
-@app.route('/people/<int:people_id>', methods=['GET'])
-def get_person(people_id):
-    person = People.query.get(people_id)
-    result = []
-    for person in people:
-        result.append({
-            'name': person.name,
-            'gender': person.gender.type if person.gender else None,
-            'specie': person.specie.languaje if person.specie else None,
-            'vehicle': person.vehicle.name if person.vehicle else None,
-            'height': person.height,
-            'films': [film.title for film in person.film]
-        })
-        return jsonify(result), 200
-    else:
-        return jsonify({'error': 'Person not found'}), 404
 
-@app.route('/planets', methods=['GET'])
+@app.route('/planets', methods=['GET']) #FUNCIONA
 def get_planets():
     planets = Planet.query.all()
     result = []
@@ -94,23 +117,23 @@ def get_planets():
         })
     return jsonify(result), 200
 
-@app.route('/planets/<int:planet_id>', methods=['GET'])
+@app.route('/planets/<int:planet_id>', methods=['GET']) #FUNCIONA
 def get_planet(planet_id):
     planet = Planet.query.get(planet_id)
-    people = People.query.all()
-    result = []
-    for planet in people:
-        result.append({
+    if planet:
+        result = {
+            'id': planet.planet_id,
             'name': planet.name,
             'population': planet.population,
             'terrain': planet.terrain,
             'diameter': planet.diameter
-        })
+        }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Planet not found'}), 404
 
-@app.route('/user/<int:user_id>/favorites', methods=['GET'])
+
+@app.route('/user/<int:user_id>/favorites', methods=['GET']) #FUNCIONA
 def get_user_favorites(user_id):
     favorites = Favorite.query.filter_by(user_id=user_id).all()
     result = []
@@ -122,16 +145,16 @@ def get_user_favorites(user_id):
         })
     return jsonify(result), 200
 
-@app.route('/favorite/planet/<int:planet_id>/user/<int:user_id>', methods=['POST'])
+@app.route('/favorite/planet/<int:planet_id>/user/<int:user_id>', methods=['POST']) #FUNCIONA
 def add_favorite_planet(planet_id, user_id):
     favorite = Favorite(user_id=user_id, planet_id=planet_id)
     db.session.add(favorite)
     db.session.commit()
     return jsonify({"message": "Favorite planet added successfully"}), 200
 
-@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
-def delete_favorite_planet(planet_id):
-    favorite = Favorite.query.filter_by(user_id=1, planet_id=planet_id).first()
+@app.route('/favorite/planet/<int:user_id>/<int:planet_id>', methods=['DELETE']) #FUNCIONA
+def delete_favorite_planet(planet_id, user_id):
+    favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
     if favorite:
         db.session.delete(favorite)
         db.session.commit()
@@ -139,16 +162,16 @@ def delete_favorite_planet(planet_id):
     else:
         return jsonify({'error': 'Favorite planet not found'}), 404
     
-@app.route('/favorite/people/<int:people_id>', methods=['POST'])
-def add_favorite_people(people_id):
-    favorite = Favorite(user_id=1, people_id=people_id)
+@app.route('/favorite/people/<int:user_id>/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id,user_id):
+    favorite = Favorite(user_id=user_id, people_id=people_id)  
     db.session.add(favorite)
     db.session.commit()
     return jsonify({"message": "Favorite people added successfully"}), 200
 
-@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
-def delete_favorite_people(people_id):
-    favorite = Favorite.query.filter_by(user_id=1, people_id=people_id).first()
+@app.route('/favorite/people/<int:user_id>/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id,user_id):
+    favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()  
     if favorite:
         db.session.delete(favorite)
         db.session.commit()
@@ -156,167 +179,157 @@ def delete_favorite_people(people_id):
     else:
         return jsonify({'error': 'Favorite people not found'}), 404
 
+    
 
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
-
-@app.route('/films', methods=['GET'])
+@app.route('/films', methods=['GET']) #FUNCIONA
 def get_films():
     films = Film.query.all()
     result = []
     for film in films:
         result.append({
+            'id': film.film_id,
             'title': film.title,
-            'director': film.director.name if film.director else None,
-            'release_date': film.release_date,
-            'opening_crawl': film.opening_crawl
+            'director': film.director_id if film.director_id else None,
+            'opening': film.opening
         })
     return jsonify(result), 200
 
-@app.route('/films/<int:film_id>', methods=['GET'])
+@app.route('/films/<int:film_id>', methods=['GET']) #FUNCIONA
 def get_film(film_id):
     film = Film.query.get(film_id)
     if film:
         result = {
+            'id': film.film_id,
             'title': film.title,
-            'director': film.director.name if film.director else None,
-            'release_date': film.release_date,
-            'opening_crawl': film.opening_crawl
+            'director': film.director_id if film.director_id else None,
+            'opening': film.opening
         }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Film not found'}), 404
 
-@app.route('/starships', methods=['GET'])
+@app.route('/starships', methods=['GET']) #FUNCIONA
 def get_starships():
     starships = Starship.query.all()
     result = []
     for starship in starships:
         result.append({
+            'id': starship.starship_id,
             'name': starship.name,
-            'model': starship.model,
-            'manufacturer': starship.manufacturer,
-            'starship_class': starship.starship_class,
-            'crew': starship.crew,
-            'passengers': starship.passengers
+            'pilot': starship.pilot,
         })
     return jsonify(result), 200
 
-@app.route('/starships/<int:starship_id>', methods=['GET'])
+@app.route('/starships/<int:starship_id>', methods=['GET']) #FUNCIONA
 def get_starship(starship_id):
     starship = Starship.query.get(starship_id)
     if starship:
         result = {
+            'id': starship.starship_id,
             'name': starship.name,
-            'model': starship.model,
-            'manufacturer': starship.manufacturer,
-            'starship_class': starship.starship_class,
-            'crew': starship.crew,
-            'passengers': starship.passengers
+            'pilot': starship.pilot,
+            
         }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Starship not found'}), 404
 
-@app.route('/vehicles', methods=['GET'])
+@app.route('/vehicles', methods=['GET']) #FUNCIONA
 def get_vehicles():
     vehicles = Vehicle.query.all()
     result = []
     for vehicle in vehicles:
         result.append({
+            'id': vehicle.vehicle_id,
             'name': vehicle.name,
             'model': vehicle.model,
-            'manufacturer': vehicle.manufacturer,
-            'vehicle_class': vehicle.vehicle_class,
-            'crew': vehicle.crew,
-            'passengers': vehicle.passengers
         })
     return jsonify(result), 200
 
-@app.route('/vehicles/<int:vehicle_id>', methods=['GET'])
+@app.route('/vehicles/<int:vehicle_id>', methods=['GET']) #FUNCIONA
 def get_vehicle(vehicle_id):
     vehicle = Vehicle.query.get(vehicle_id)
     if vehicle:
         result = {
+            'id': vehicle.vehicle_id,
             'name': vehicle.name,
             'model': vehicle.model,
-            'manufacturer': vehicle.manufacturer,
-            'vehicle_class': vehicle.vehicle_class,
-            'crew': vehicle.crew,
-            'passengers': vehicle.passengers
         }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Vehicle not found'}), 404
 
-@app.route('/genders', methods=['GET'])
+@app.route('/genders', methods=['GET']) #FUNCIONA
 def get_genders():
     genders = Gender.query.all()
     result = []
     for gender in genders:
         result.append({
-            'id': gender.id,
+            'id': gender.gender_id,
             'type': gender.type
         })
     return jsonify(result), 200
 
-@app.route('/genders/<int:gender_id>', methods=['GET'])
+@app.route('/genders/<int:gender_id>', methods=['GET']) #FUNCIONA
 def get_gender(gender_id):
     gender = Gender.query.get(gender_id)
     if gender:
         result = {
-            'id': gender.id,
+            'id': gender.gender_id,
             'type': gender.type
         }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Gender not found'}), 404
 
-@app.route('/species', methods=['GET'])
+@app.route('/species', methods=['GET']) #FUNCIONA
 def get_species():
     species = Specie.query.all()
     result = []
     for specie in species:
         result.append({
-            'id': specie.id,
-            'language': specie.language,
-            'classification': specie.classification
+            'id': specie.specie_id,
+            'languaje': specie.languaje,
         })
     return jsonify(result), 200
 
-@app.route('/species/<int:specie_id>', methods=['GET'])
+@app.route('/species/<int:specie_id>', methods=['GET']) #FUNCIONA
 def get_specie(specie_id):
     specie = Specie.query.get(specie_id)
     if specie:
         result = {
-            'id': specie.id,
-            'language': specie.language,
-            'classification': specie.classification
+            'id': specie.specie_id,
+            'languaje': specie.languaje,
         }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Specie not found'}), 404
 
-@app.route('/directors', methods=['GET'])
+@app.route('/directors', methods=['GET']) #FUNCIONA
 def get_directors():
     directors = Director.query.all()
     result = []
     for director in directors:
         result.append({
-            'id': director.id,
+            'id': director.directo_id,
             'name': director.name
         })
     return jsonify(result), 200
 
-@app.route('/directors/<int:director_id>', methods=['GET'])
+@app.route('/directors/<int:director_id>', methods=['GET']) #FUNCIONA
 def get_director(director_id):
     director = Director.query.get(director_id)
     if director:
         result = {
-            'id': director.id,
+            'id': director.directo_id,
             'name': director.name
         }
         return jsonify(result), 200
     else:
         return jsonify({'error': 'Director not found'}), 404
+
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
+
